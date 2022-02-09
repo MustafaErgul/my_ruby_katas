38ea3e52665087490913ef962b747f10872dd8dc
Mustafa Ergül <mustafa@travis-ci.org>
2022-02-09 18:02:30 +0300

    could be simpler but..

---
+++ simple_pig_latin.rb
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/520b9d2ad5c005041100000f
+
+def pig(str)
+  arr2 = str.split(' ').map do |s|
+    arr = s.split(' ').map do |ch|
+      chs = ch.chars
+      f = chs.first
+      chs.delete_at(chs.find_index(chs[0])) && chs.insert(-1, f).join("") + "ay"
+    end   
+  end if str !=~ /[[:punct:]]/
+  p arr2.concat.join(' ') 
+end
+
+
+pig('Pig latin is cool')
