5c82d58e970f2f61aca56d6216af26e95777c1c6
mustafaergul <mustafaergul0@gmail.com>
2022-02-24 21:31:30 +0300

    like this one

---
+++ very_even_numbers.rb
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/58c9322bedb4235468000019/train/ruby
+
+def very_even(n)
+  n = n.digits.sum while n.digits.size != 1
+  p n.digits.sum.even? ? true : false 
+end
+
+
+very_even 88
+very_even 222
+very_even 841
